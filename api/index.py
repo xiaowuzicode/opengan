fa637d50b717577ecef0684dc6a9bd981362ef4c
BoyNaive <30629302+xiaowuzicode@users.noreply.github.com>
2023-07-25 19:38:03 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,16 +9,7 @@ openai.api_key = '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'
 @app.route('/chat' methods=['POST'])
 def chat():
     user_input = request.form['input']
-    response = openai.Completion.create(
-    engine='text-davinci-003'
-    prompt=user_input
-    max_tokens=50
-    temperature=0.7
-    n=1
-    stop=None
-    temperature=0.7
-    )
-    return response.choices[0].text.strip()
+    return 'sdasdschat'
     
 @app.route('/')
 def home():
