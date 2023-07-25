86dc3b86f47b4200ab259796dbe4d01febfcc3fa
BoyNaive <30629302+xiaowuzicode@users.noreply.github.com>
2023-07-25 20:03:04 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,23 +2,6 @@ from flask import Flask,request
 import openai
 import os
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-app = Flask(__name__)
-
-@app.route('/chat' methods=['POST'])
-def chat():
-    user_input = request.form['user_input']
-    response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=user_input,
-            temperature=0.7,
-            frequency_penalty=0,
-            presence_penalty=0.7,
-            max_tokens=240
-        )
-    return response.choices[0].text.strip()
-
     
 @app.route('/')
 def home():
