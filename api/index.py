cd8079d9000b6dfcb48740786254698b481e499e
BoyNaive <30629302+xiaowuzicode@users.noreply.github.com>
2023-07-25 17:40:02 +0800

    Create index.py
    路径问题坑死人

---
+++ api/index.py
@@ -0,0 +1,24 @@
+from flask import Flask render_template request
+import openai
+
+app = Flask(__name__)
+
+openai.api_key = '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'
+
+@app.route('/')
+def home():
+    return 'Hello'
+
+@app.route('/chat' methods=['POST'])
+def chat():
+    user_input = request.form['input']
+    response = openai.Completion.create(
+        engine='text-davinci-003'
+        prompt=user_input
+        max_tokens=50
+        temperature=0.7
+        n=1
+        stop=None
+        temperature=0.7
+    )
+    return response.choices[0].text.strip()
