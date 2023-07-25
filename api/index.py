0b070bcb5468665c2d081c8906ee5cd8e166d4f2
BoyNaive <30629302+xiaowuzicode@users.noreply.github.com>
2023-07-25 17:45:50 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,6 @@
-from flask import Flask render_template request
-import openai
+from flask import Flask
 
 app = Flask(__name__)
-
-openai.api_key = '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'
 
 @app.route('/')
 def home():
@@ -12,17 +9,3 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
-
-@app.route('/chat' methods=['POST'])
-def chat():
-    user_input = request.form['input']
-    response = openai.Completion.create(
-        engine='text-davinci-003'
-        prompt=user_input
-        max_tokens=50
-        temperature=0.7
-        n=1
-        stop=None
-        temperature=0.7
-    )
-    return response.choices[0].text.strip()
