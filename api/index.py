eed762aaa4f7e26605e9d0555f2eea1a5b9f6757
BoyNaive <30629302+xiaowuzicode@users.noreply.github.com>
2023-07-25 17:48:26 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask request
+import openai
 
 app = Flask(__name__)
+openai.api_key = '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'
 
 @app.route('/')
 def home():
