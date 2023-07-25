2564f9e74e0e7df4c9af26e906683196209c11a4
BoyNaive <30629302+xiaowuzicode@users.noreply.github.com>
2023-07-25 21:12:04 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,8 @@ import os
 
 app = Flask(__name__)
 
-openai.api_key = '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'
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 
 @app.route('/chat',methods=["POST"])
 def chat():
