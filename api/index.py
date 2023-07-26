e97b066bdb510eca0b7a5c05b4d41d8d945442f6
陈刚 <chengang_c@aspirecn.com>
2023-07-26 23:31:19 +0800

    封装一下

--- api/index.py
+++ api/index.py
@@ -1,33 +1,65 @@
-from flask import Flask,request,jsonify
+from flask import Flask,request
 import openai
 import os
+import json
+import traceback
 
 app = Flask(__name__)
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def Response_headers(content):  
-    resp = Response(content)  
-    resp.headers['Access-Control-Allow-Origin'] = '*'  
-    return resp
-
-
 @app.route('/chat',methods=["POST"])
 def chat():
-    data = request.get_json()
-    user_input = data['userInput']
-    userPassword = data['userPassword']
+    try:
+        print(request.method)
+        if request.method == "GET":
+            content = request.args.get("content")
+            # comment = request.values.get("content")
+            res = int(content) + 10
+            print(res, type(content))
+
+        elif request.method == "POST":
+            print("========", request.headers)
+            content_type = request.headers.get('Content-Type')
+            if "multipart/form-data" in content_type:
+                form_data = dict(request.form)
+                # files_data = dict(request.files)
+                # print(form_data)
+
+                res = int(form_data.get('content')) + 14
+
+            elif "application/json" in content_type:
+                # request.get_data() # 原始的数据
+                input_dict = request.get_json()
+                res = input_dict.get('content')
+                print(res)
+            elif "application/x-www-form-urlencoded" in content_type:
+                input_dict = request.form
+                # request.values.get("content")
+            else:
+                print(request.get_data())
+
+            # 调用 API 生成文本
+            outContent = generate_text(res)
+            print(outContent)
+        print('url: %s , script_root: %s , path: %s , base_url: %s , url_root : %s' % (
+            request.url, request.script_root, request.path, request.base_url, request.url_root))
+        return json.dumps({"code": 0, "msg": "success", "data": outContent},ensure_ascii=False)
+    except:
+        err_msg = 'url: %s, err_msg: %s' % (request.url, (str(traceback.format_exc())))
+        print(err_msg)
+        return json.dumps({"code": -1, "msg": "failed", "data": 0})
+
+# 调用 GPT-3.5-turbo API 来生成文本
+def generate_text(prompt):
     response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=user_input,
-            temperature=0.7,
-            frequency_penalty=0,
-            presence_penalty=0.7,
-            max_tokens=240
+        engine="text-davinci-003",  # 使用 GPT-3.5-turbo 引擎
+        prompt=prompt,
+        max_tokens=240,  # 生成的最大文本长度
+        stop=None,  # 可选：在此停止生成文本的标记列表
+        temperature=0.7,  # 控制生成文本的创造性。较低的值会使输出更加保守，较高的值会使输出更加随机。
     )
-    content = response.choices[0].text.strip()
-    resp = Response_headers(content)  
-    return resp
+    return response.choices[0].text.strip()
 
 @app.route('/test',methods=["POST"])
 def test():
@@ -43,3 +75,6 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+if __name__ == '__main__':
+    app.run("127.0.0.1", debug=True, port=6006)
